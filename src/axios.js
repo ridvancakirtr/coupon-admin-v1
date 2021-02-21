6c195d173ed59c7af5080c9b256d0bfa9bc5b28f
RIDVAN ÇAKIR <ridvancakirtr@gmail.com>
2021-02-21 14:21:07 +0300

    heroku

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 //const baseURL = 'http://localhost:1010/api/v1/'
-const baseURL = 'https://coupon-server-v1.herokuapp.com/'
+const baseURL = 'https://coupon-server-v1.herokuapp.com/api/v1/'
 export default axios.create({
   baseURL,
   headers: {
